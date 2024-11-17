87648b72ccb1ebcee7aae012ffd1cd6908c80dab
arshemii <arshemii1373.2@gmail.com>
2024-11-17 14:21:35 +0100

    New inference file

---
+++ infer_utils.py
@@ -0,0 +1,295 @@
+"""
+Created on Fri Nov 15 15:51:11 2024
+
+@author: arash
+"""
+import openvino as ov
+import openvino.properties.intel_cpu as intel_cpu
+import openvino.properties.hint as hints
+import pyrealsense2 as rs
+import cv2
+import numpy as np
+from ultralytics.utils import yaml_load
+from ultralytics.utils.checks import check_yaml
+
+
+class FlexInfer():
+    
+    
+    
+    
+    def __init__(self, ):
+        self.DEVICE = "CPU"
+        self.CONFIG = {hints.performance_mode: hints.PerformanceMode.THROUGHPUT}
+        self.CLASSES = yaml_load(check_yaml("coco8.yaml"))["names"]
+        self.colors = np.random.uniform(0, 255, size=(len(self.CLASSES), 3))
+        self.s_nms = 0.45
+        self.rgb_ints = {
+            'fx': 617.323486328125,
+            'fy': 617.6768798828125,
+            'ppx': 330.5740051269531,
+            'ppy': 235.93508911132812,
+            }   
+        
+    def RS(self):
+        """
+        This function initialize the stream of realsense camera
+        """
+
+        pipe = rs.pipeline()
+        cfg = rs.config()
+        cfg.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)  # RGB stream
+        cfg.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)  # Depth stream
+            
+        # Start the pipeline
+        profile = pipe.start(cfg)
+        device = profile.get_device()
+        depth_sensor = device.first_depth_sensor()
+        depth_scale = depth_sensor.get_depth_scale()
+        
+        
+        # Create an align object
+        align = rs.align(rs.stream.color)  # Align depth to color
+            
+        return pipe, align, depth_scale
+    
+            
+    def WC(self):
+        cap = cv2.VideoCapture(0)
+        if not cap.isOpened():
+            print("Error: Could not open webcam.")
+            
+        return cap
+    
+    
+    
+    
+    def cmodel(self, model):
+        
+        model_path = f"./models/{model}_openvino_model/{model}.xml"
+        core = ov.Core()
+        core.set_property("CPU", intel_cpu.denormals_optimization(True))
+        core.set_property("CPU", {hints.execution_mode: hints.ExecutionMode.PERFORMANCE})
+        
+        # Uncomment if accuracy matters
+        #core.set_property("CPU", {hints.execution_mode: hints.ExecutionMode.ACCURACY})
+        
+        compiled_model = core.compile_model(model_path, device_name=self.DEVICE, config=self.CONFIG)
+        #compiled_model = core.compile_model(model_path, device_name=device)
+        
+        
+        return compiled_model
+
+    def wc_preprocess(self, frame, input_tuple):
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.resize(frame, (input_tuple[2], input_tuple[3]))
+        frame = frame.astype(np.float32) / 255.0
+        frame = np.transpose(frame, (2, 0, 1))  # (3, 640, 640)
+        frame = np.expand_dims(frame, axis=0)
+        
+        return frame
+    
+    
+    def rs_preprocess(self, color, depth, input_tuple, depth_scale):
+        color = np.asanyarray(color.get_data())
+        depth = np.asanyarray(depth.get_data())
+        depth = depth * depth_scale
+        color = cv2.resize(color, (input_tuple[2], input_tuple[3]))
+        depth = cv2.resize(depth, (input_tuple[2], input_tuple[3]))
+        color = color.astype(np.float32) / 255.0
+        color = np.transpose(color, (2, 0, 1))  # (3, 640, 640)
+        color = np.expand_dims(color, axis=0)
+        
+        return color, depth
+    
+    
+    def intrinsic(self, box_center, depth, rgb_ints):
+        fx = rgb_ints['fx']
+        fy = rgb_ints['fy']
+        ppx = rgb_ints['ppx']
+        ppy = rgb_ints['ppy']
+        
+        dcm = 100*depth[box_center[0], box_center[1]]
+        cxcm = (box_center[0] - ppx) * dcm / fx
+        cycm = (box_center[1] - ppy) * dcm / fy
+        
+        return cxcm, cycm, dcm
+    
+    
+    def draw_bounding_box(self, img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+        """
+        Draws bounding boxes on the input image based on the provided arguments.
+
+        Args:
+            img (numpy.ndarray): The input image to draw the bounding box on.
+            class_id (int): Class ID of the detected object.
+            confidence (float): Confidence score of the detected object.
+            x (int): X-coordinate of the top-left corner of the bounding box.
+            y (int): Y-coordinate of the top-left corner of the bounding box.
+            x_plus_w (int): X-coordinate of the bottom-right corner of the bounding box.
+            y_plus_h (int): Y-coordinate of the bottom-right corner of the bounding box.
+        """
+        
+        label = f"{self.CLASSES[class_id]} ({confidence:.2f})"
+        color = self.colors[class_id]
+        cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
+        cv2.putText(img, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    
+    
+    
+    def od_vis(self, original_image, res, conf, iou):
+        outputs = np.array([cv2.transpose(res[0])])
+        rows = outputs.shape[1]
+
+        boxes = []
+        scores = []
+        class_ids = []
+        
+        original_image = np.array(255*original_image, dtype=np.uint8)
+        original_image = np.transpose(original_image[0], (1, 2, 0))
+        
+            # Iterate through output to collect bounding boxes, confidence scores, and class IDs
+        for i in range(rows):
+            classes_scores = outputs[0][i][4:]
+            (minScore, maxScore, minClassLoc, (x, maxClassIndex)) = cv2.minMaxLoc(classes_scores)
+            if maxScore >= conf:
+                box = [
+                    outputs[0][i][0] - (0.5 * outputs[0][i][2]),
+                    outputs[0][i][1] - (0.5 * outputs[0][i][3]),
+                    outputs[0][i][2],
+                    outputs[0][i][3],
+                ]
+                boxes.append(box)
+                scores.append(maxScore)
+                class_ids.append(maxClassIndex)
+
+            # Apply NMS (Non-maximum suppression)
+        result_boxes = cv2.dnn.NMSBoxes(boxes, scores, conf, iou, self.s_nms)
+
+        detections = []
+
+            # Iterate through NMS results to draw bounding boxes and labels
+        for i in range(len(result_boxes)):
+            index = result_boxes[i]
+            box = boxes[index]
+            detection = {
+                "class_id": class_ids[index],
+                "class_name": self.CLASSES[class_ids[index]],
+                "confidence": scores[index],
+                "box": box,
+            }
+            detections.append(detection)
+            
+            self.draw_bounding_box(
+                original_image,
+                class_ids[index],
+                scores[index],
+                round(box[0]),
+                round(box[1]),
+                round((box[0] + box[2])),
+                round((box[1] + box[3])),
+            )
+
+        return original_image
+
+
+    def od_report(self, res, conf, iou):
+        outputs = np.array([cv2.transpose(res[0])])
+        rows = outputs.shape[1]
+
+        boxes = []
+        scores = []
+        class_ids = []
+        
+            # Iterate through output to collect bounding boxes, confidence scores, and class IDs
+        for i in range(rows):
+            classes_scores = outputs[0][i][4:]
+            (minScore, maxScore, minClassLoc, (x, maxClassIndex)) = cv2.minMaxLoc(classes_scores)
+            if maxScore >= conf:
+                box = [
+                    outputs[0][i][0] - (0.5 * outputs[0][i][2]),
+                    outputs[0][i][1] - (0.5 * outputs[0][i][3]),
+                    outputs[0][i][2],
+                    outputs[0][i][3],
+                ]
+                boxes.append(box)
+                scores.append(maxScore)
+                class_ids.append(maxClassIndex)
+
+            # Apply NMS (Non-maximum suppression)
+        result_boxes = cv2.dnn.NMSBoxes(boxes, scores, conf, iou, self.s_nms)
+
+        detections = []
+        for i in range(len(result_boxes)):
+            box = boxes[i]
+            box_center_x = round(box[0]+box[2]/2)
+            box_center_y = round(box[1]+box[3]/2)
+            index = result_boxes[i]
+            name = self.CLASSES[class_ids[index]]
+            confidence = scores[index]
+            detections.append([box_center_x, box_center_y, name, f"{confidence:.2f}"])
+            
+        detection_strings = []
+
+        for detection in detections:
+            name = detection[2]
+            x = detection[0]
+            y = detection[1]
+            c = detection[3]
+            detection_strings.append(f"{name} in ({x}, {y}) with conf: {c}")
+
+        print(", ".join(detection_strings) + "----------------------------------------------")
+        
+        return detections
+        
+        
+        
+        
+
+    def pose_est(self, res, depth, conf, iou):
+        outputs = np.array([cv2.transpose(res[0])])
+        rows = outputs.shape[1]
+
+        boxes = []
+        scores = []
+        class_ids = []
+        for i in range(rows):
+            classes_scores = outputs[0][i][4:]
+            (minScore, maxScore, minClassLoc, (x, maxClassIndex)) = cv2.minMaxLoc(classes_scores)
+            if maxScore >= conf:
+                box = [
+                    outputs[0][i][0] - (0.5 * outputs[0][i][2]),
+                    outputs[0][i][1] - (0.5 * outputs[0][i][3]),
+                    outputs[0][i][2],
+                    outputs[0][i][3],
+                ]
+                boxes.append(box)
+                scores.append(maxScore)
+                class_ids.append(maxClassIndex)
+
+        # Apply NMS (Non-maximum suppression)
+        result_boxes = cv2.dnn.NMSBoxes(boxes, scores, conf, iou, self.s_nms)
+        detections = []
+        for i in range(len(result_boxes)):
+            box = boxes[i]
+            box_center = (round(box[0]+box[2]/2), round(box[1]+box[3]/2))
+            center_x_cm, center_y_cm, depth_cm = self.intrinsic(box_center, depth, self.rgb_ints)
+            index = result_boxes[i]
+            name = self.CLASSES[class_ids[index]]
+            confidence = scores[index]
+            detections.append([center_x_cm, center_y_cm, depth_cm, name, f"{confidence:.2f}"])
+            
+        detection_strings = []
+
+        for detection in detections:
+            name = detection[3]
+            x = detection[0]
+            y = detection[1]
+            d = detection[2]
+            c = detection[4]
+            detection_strings.append(f"{name} in ({x}, {y}, {d}) with conf: {c}")
+
+        print(", ".join(detection_strings) + "----------------------------------------------")
+            
+        return detections
+        
---
+++ main.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 15 15:53:22 2024
+
+@author: arash
+"""
+
+from infer_utils import FlexInfer
+import argparse
+import openvino as ov
+import time
+import cv2
+
+def main(model, vis, capture, conf, iou, mapping):
+    flex = FlexInfer()
+    
+    cmodel = flex.cmodel(model)
+    
+    a_infer = cmodel.create_infer_request()
+    b_infer = cmodel.create_infer_request()
+
+    a_infer.share_inputs = True
+    a_infer.share_outputs = True
+    b_infer.share_inputs = True
+    b_infer.share_outputs = True
+    
+    input_layer = cmodel.input(0)
+    input_shape = cmodel.input(0).shape
+    input_tuple = (input_shape[0], input_shape[1], input_shape[2], input_shape[3])
+    
+    
+    if capture == "webcam":
+        cap = flex.WC()
+        _, frame = cap.read(0)
+        frame = flex.wc_preprocess(frame, input_tuple)
+        
+    if capture == "realsense":
+        pipe, align, depth_scale = flex.RS()
+        frameset = pipe.wait_for_frames()
+        aligned_frames = align.process(frameset)
+        depth_frame = aligned_frames.get_depth_frame()
+        color_frame = aligned_frames.get_color_frame()
+        frame, depth = flex.rs_preprocess(color_frame, depth_frame, input_tuple, depth_scale)
+        
+    async_fps = 0
+    a_infer.set_tensor(input_layer, ov.Tensor(frame))
+    a_infer.start_async()
+    
+    while True:
+        ti = time.time()
+        if capture == "webcam":
+            _, frame_next = cap.read()
+            frame_next = flex.wc_preprocess(frame_next, input_tuple)
+            
+        if capture == "realsense":
+            frameset = pipe.wait_for_frames()
+            aligned_frames = align.process(frameset)
+            depth_frame = aligned_frames.get_depth_frame()
+            color_frame = aligned_frames.get_color_frame()
+            frame_next, depth_next = flex.rs_preprocess(color_frame, depth_frame, input_tuple)
+            
+        b_infer.set_tensor(input_layer, ov.Tensor(frame_next))
+        b_infer.start_async()
+        a_infer.wait()
+        res = a_infer.get_output_tensor(0).data
+        
+        total_time = time.time() - ti
+        async_fps = 1 / total_time #Absolute, not average
+        print(f"Absolute FPS: {async_fps}")
+        
+        if capture == "webcam":
+            if vis == "video":
+                # Mode is object detection visualization
+                frame_next = flex.od_vis(frame_next, res, conf, iou)
+                cv2.imshow("image", frame_next)
+                key = cv2.waitKey(1)
+                # escape = 27
+                if key == 27:
+                    break
+            if vis == "text":
+                # Mode is object detection with no visualization
+                detections = flex.od_report(res, conf, iou)
+        if capture == "realsense":
+            # Mode is pose estimation (Report)
+            detections = flex.pose_est(res, depth_next, conf, iou, mapping)
+            
+        frame = frame_next
+        a_infer, b_infer = b_infer, a_infer
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Object Detection with OpenVINO")
+    
+    # Model selection
+    parser.add_argument(
+        "--model", 
+        default="yolov8n_quantized", 
+        help="Input your OV model path or name."
+    )
+
+    
+    
+    parser.add_argument(
+        "--vis", 
+        default="video", 
+        choices=["video", "text"], 
+        help="Choose if you need visualization or textual report (default: video)."
+    )
+    
+    # Capture source
+    parser.add_argument(
+        "--capture", 
+        default="webcam", 
+        choices=["webcam", "realsense"], 
+        help="Choose between webcam or Intel RealSense as the capture source (default: webcam)."
+    )
+    
+    
+    # Confidence Score
+    parser.add_argument(
+        "--conf", 
+        type=float, 
+        default=0.25, 
+        help="Confidence score threshold for detections (default: 0.25)."
+    )
+    
+    # IoU
+    parser.add_argument(
+        "--iou", 
+        type=float, 
+        default=0.45, 
+        help="Minimum IoU threshold for non-maximum suppression (default: 0.45)."
+    )
+    
+    args = parser.parse_args()
+    main(args.model, args.vis, args.capture, args.conf, args.iou)
