ec359948ad7167f302caee600a07489db04f8e6a
arshemii <arshemii1373.2@gmail.com>
2024-11-25 15:34:58 +0100

    Update infer_utils.py
    Add a missing function definition

--- infer_utils.py
+++ infer_utils.py
@@ -9,15 +9,27 @@ import openvino.properties.hint as hints
 import pyrealsense2 as rs
 import cv2
 import numpy as np
-from ultralytics.utils import yaml_load
-from ultralytics.utils.checks import check_yaml
+from pathlib import Path
+import re
+import yaml
 
 
-class FlexInfer():
-    
-    
-    
-    
+def yaml_load(file="data.yaml", append_filename=False):
+    assert Path(file).suffix in {".yaml", ".yml"}, f"Attempting to load non-YAML file {file} with yaml_load()"
+    with open(file, errors="ignore", encoding="utf-8") as f:
+        s = f.read()  # string
+        # Remove special characters
+        if not s.isprintable():
+            s = re.sub(r"[^\x09\x0A\x0D\x20-\x7E\x85\xA0-\uD7FF\uE000-\uFFFD\U00010000-\U0010ffff]+", "", s)
+        # Add YAML filename to dict and return
+        data = yaml.safe_load(s) or {}  # always return a dict (yaml.safe_load() may return None for empty files)
+        if append_filename:
+            data["yaml_file"] = str(file)
+        return data
+
+
+
+class FlexInfer():    
     def __init__(self, ):
         self.DEVICE = "CPU"
         self.CONFIG = {hints.performance_mode: hints.PerformanceMode.THROUGHPUT}
