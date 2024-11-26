6beb75a0cf93832ed7288feacd21539aba44f2a2
arshemii <arshemii1373.2@gmail.com>
2024-11-26 16:07:16 +0100

    Create docker_run.sh

---
+++ docker_run.sh
@@ -0,0 +1,9 @@
+  docker run -it --rm \
+  --user root \
+  --device /dev/video0:/dev/video0 \  # Enable access to webcam
+  # add other devices (If use Realsense D435, you need all available devices)
+  -e QT_QPA_PLATFORM=xcb \
+  -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
+  --cpus="2.0" \     # Adjust it if you need a different limitaion
+  --memory="2g" \    # Adjust it if you need a different limitaion
+  arshemii/drone_od:25nov24
